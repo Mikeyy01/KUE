c219f6508b34a64c43211de09e838cf78ff64bec
Steph <s.espiritu@student.fontys.nl>
2023-01-07 09:59:55 +0100

    styled the DJ page connection

--- React/todo/src/components/TodoList.js
+++ React/todo/src/components/TodoList.js
@@ -2,6 +2,10 @@ import Confirm from './Confirm';
 import '../assets/css/list.css';
 import {useState, useEffect} from "react";
 import axios from "axios";
+import acceptBtn from '../assets/images/accept.png'
+import declineBtn from '../assets/images/decline.png'
+import logoHeader from '../assets/images/logo.png'
+
 const { REACT_APP_BACKEND_URL } = process.env
 
 
@@ -105,44 +109,73 @@ const toggleComplete = todo => {
             });
     };
 
-
+// html part
 
     return <div>
-        {todos.map((todo) =>
-            editableTodo && editableTodo.id === todo.id ? (
-                    <div className="todo" key={todo.id} style={{ display: "flex", margin: "8px" }}>
-            <form onSubmit={handleEditTodo}>
-                <input
-                    type="text"
-                    value={editableTodo.todo}
-                    onChange={(event) =>
-                    setEditableTodo({
-                    ...editableTodo,
-                    todo: event.target.value,
-                })
-                }
-                />
-            </form>
-                <button onClick={() => setEditableTodo(null)}>X</button>
-                    </div>
-                ) : (
-                <div key={todo.id} className="todo" style={{ display: "flex", margin: "8px" }}>
-                    <h3 style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
-                        {todo.todo.artist}
-                    </h3>
-                    <p style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
-                        {todo.todo.track}
-                    </p>
-                    <p style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
-                        {todo.timestamp}
-                    </p>
-                    <img src={todo.todo.coverArtURL} alt="cover art" />
-                    <button onClick={() => toggleComplete(todo)} className="accept-button">V</button>
-                    <button onClick={() => handleDelete(todo)} className="delete-button">X</button>
-                </div>
-        ))}
-    </div>
-};
+        <div className='containerBody'>
+    
+            <div className='todoBody'>
+                <header className='headerLogo'>
+                <img className= "logoHdr" src={logoHeader} width ="30%" height="30%" />
+                <h2>Welcome DJ&nbsp;</h2>
+                <h2 id="djcode"></h2>
+                
+                 </header>
+
+                <div className='dropDown'>
+                     <select className='drpDwn'>
+                         <option value = "0" selected >All</option>
+                        <option value = "1" >Requested</option>
+                        <option value = "2" >Rejected</option>
+                        <option value = "3" >Accepted</option>
+                     </select>
+                
+                 </div>
+
+                    {todos.map((todo) =>
+                        editableTodo && editableTodo.id === todo.id ? (
+                                <div className="todo" key={todo.id} style={{ display: "flex", margin: "8px" }}>
+                        <form onSubmit={handleEditTodo}>
+                            <div className='selection-box'>
+                                <input 
+                                type="text"
+                                value={editableTodo.todo}
+                                onChange={(event) =>
+                                setEditableTodo({
+                                ...editableTodo,
+                                todo: event.target.value,
+                                })
+                            }
+                            /></div>
+                            
+                        </form>
+                            <button onClick={() => setEditableTodo(null)}>X</button>
+                                </div>
+                            ) : (
+                            <div key={todo.id} className="todo" style={{ display: "flex", margin: "20px" , justifyContent : 'center' , alignItems: 'center'}}>
+                                <img className="albumCover"src={todo.todo.coverArtURL} alt="cover art" /> 
+                                <h3 className="artistName" style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
+                                    {todo.todo.artist}
+                                </h3>
+                                <p className="songName"style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
+                                    {todo.todo.track}
+                                </p>
+                                <p className="timeStamp" style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
+                                    {todo.timestamp}
+                                </p>
+                                
+                            <button onClick={() => toggleComplete(todo)} className="accept-button"> <img className= "aBtn" src={acceptBtn} mix-blend-mode ="multiply" width ="25%" height="25%" /></button>
+                                <button onClick={() => handleDelete(todo)} className="delete-button"><img className= "dBtn" src={declineBtn} width ="25%" height="25%" /></button>
+                            </div>
+                    ))}
+              
+            </div>
+        
+       </div>
+    
+     </div>
+    
+    };
 
 export default TodoList;
 
